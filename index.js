aa3b9aa5f4476e45a39d74d41989118669701048
joezanini <jzanini@ucsc.edu>
2022-08-02 16:45:22 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const COOKIE_NAME = "webex_kitchen_sink_cookie";
+const LOCAL_STORAGE_NAME = "webex_kitchen_sink_localstorage";
+const SESSION_STORAGE_NAME = "webex_kitchen_sink_sessionstorage";
+
+var app = new window.Webex.Application();
+
+app.onReady().then(() => {
+    log('onReady()', { message: 'host app is ready' })
+    app.listen().then(() => {
+        app.on('application:displayContextChanged', (payload) => log('application:displayContextChanged', payload));
+        app.on('application:shareStateChanged', (payload) => log('application:shareStateChanged', payload));
+        app.on('application:themeChanged', (payload) => log('application:themeChanged', payload));
+        app.on('meeting:infoChanged', (payload) => log('meeting:infoChanged', payload));
+        app.on('meeting:roleChanged', (payload) => log('meeting:roleChanged', payload));
+        app.on('space:infoChanged', (payload) => log('space:infoChanged', payload));
+    })
+});
+
+function manageUserView() {
+    app.context.getMeeting().then((m) => {
+        log('getMeeting()', m);
+    }).catch((error) => {
+        log('getMeeting() promise failed with error', Webex.Application.ErrorCodes[error]);
+    });
+}
+
+function log(type, data) {
+    var ul = document.getElementById("console");
+    var li = document.createElement("li");
+    var payload = document.createTextNode(`${type}: ${JSON.stringify(data)}`);
+    li.appendChild(payload)
+    ul.prepend(li);
+}
+
+manageUserView();
